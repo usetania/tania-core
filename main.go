ae7ec5043ea60cda0134523cf469cca9b103b5a5
Purwandi <free6300@gmail.com>
2017-12-13 07:30:20 +0700

    Bootstraping project
    - Add database migrations
    - Add frontend scripts

--- main.go
+++ main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
+	"database/sql"
+	"io/ioutil"
+
 	"github.com/Tanibox/tania-server/routing"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	// Group for Farms HTTP routing
-	farms := e.Group("/farms")
-	farms.GET("/", routing.FarmsIndex)
-	farms.POST("/", routing.FarmsCreate)
-	farms.GET("/:uid", routing.FarmsShow)
-	farms.PUT("/:uid", routing.FarmsUpdate)
-	farms.DELETE("/:uid", routing.FarmsDestroy)
+	// Bootstraping Database
+	db := initDB("resources/storage.db")
+	migrate(db)
 
-	// Group for Areas HTTP routing
-	areas := e.Group("/areas")
-	areas.GET("/", routing.AreasIndex)
-	areas.POST("/", routing.AreasCreate)
-	areas.POST("/:uid/harvest", routing.AreasHarvest)
-	areas.POST("/:uid/dispose", routing.AreasDispose)
-	areas.DELETE("/:uid", routing.AreasDestroy)
+	// HTTP routing
+	API := e.Group("api")
+	API.Use(middleware.CORS())
+	routing.AreasRouter(API.Group("/farms"))
+	routing.FarmsRouter(API.Group("/areas"))
+	routing.ReservoirsRouter(e.Group("/reservoirs"))
 
-	// Group for Reservoirs HTTP routing
-	reservoirs := e.Group("/reservoirs")
-	reservoirs.GET("/", routing.ReservoirsIndex)
+	e.Static("/", "public")
+	e.Logger.Fatal(e.Start(":8080"))
+}
 
-	e.Logger.Fatal(e.Start(":1323"))
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nill")
+	}
+
+	return db
+}
+
+func migrate(db *sql.DB) {
+	filerc, err := ioutil.ReadFile("resources/structure.sql")
+	if err != nil {
+		panic(err)
+	}
+	sql := string(filerc)
+
+	_, err = db.Exec(sql)
+
+	if err != nil {
+		panic(err)
+	}
 }
