160b19b3c80a39fdec91292811520af0a40777d3
Adhatama <am.adhatama@gmail.com>
2017-12-20 19:40:35 +0700

    Enhance reservoir repository. But found weird case on test: when running test multiple times, it can failed once at a time.

--- reservoir/repository/reservoir_repository_test.go
+++ reservoir/repository/reservoir_repository_test.go
@@ -9,25 +9,40 @@ import (
 
 func TestInMemorySave(t *testing.T) {
 	// Given
-	repo := ReservoirRepositoryInMemory{}
-	reservoir, _ := reservoir.CreateReservoir("My Reservoir 1", 8.1, 12.2, 30.0)
+	var repo ReservoirRepository = &ReservoirRepositoryInMemory{}
+
+	reservoir1, _ := reservoir.CreateReservoir("My Reservoir 1", 8.1, 12.2, 30.0)
+
+	reservoir2, _ := reservoir.CreateReservoir("My Reservoir 2", 8.1, 12.2, 30.0)
+	bucket2, _ := reservoir.CreateBucket(100, 50)
+	reservoir2.AttachBucket(&bucket2)
 
 	// When
-	uid := repo.Save(reservoir)
+	uid1, _ := repo.Save(reservoir1)
+	uid2, _ := repo.Save(reservoir2)
 
 	// Then
-	assert.NotNil(t, uid)
+	assert.NotNil(t, uid1)
+
+	assert.NotNil(t, uid2)
+	assert.NotNil(t, uid2)
 }
 
 func TestInMemoryFindAll(t *testing.T) {
 	// Given
-	repo := ReservoirRepositoryInMemory{}
-	reservoir, _ := reservoir.CreateReservoir("My Reservoir 1", 8.1, 12.2, 30.0)
-	repo.Save(reservoir)
+	var repo ReservoirRepository = &ReservoirRepositoryInMemory{}
+
+	reservoir1, _ := reservoir.CreateReservoir("My Reservoir 1", 8.1, 12.2, 30.0)
+	reservoir2, _ := reservoir.CreateReservoir("My Reservoir 2", 8.1, 12.2, 30.0)
+	reservoir3, _ := reservoir.CreateReservoir("My Reservoir 3", 8.1, 12.2, 30.0)
+	repo.Save(reservoir1)
+	repo.Save(reservoir2)
+	repo.Save(reservoir3)
 
 	// When
-	reservoirs := repo.FindAll()
+	reservoirs, _ := repo.FindAll()
 
 	// Then
-	assert.Equal(t, len(reservoirs), 1)
+	assert.Equal(t, len(reservoirs), 3)
+	assert.Equal(t, reservoirs[0].Name, reservoir1.Name)
 }
