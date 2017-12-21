8c1ae62ca22a60d2d92d5107a7cceab73b6e854c
Adhatama <am.adhatama@gmail.com>
2017-12-21 13:57:41 +0700

    Use goroutine in reservoir repository. But the test still not working

--- reservoir/repository/reservoir_repository.go
+++ reservoir/repository/reservoir_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"math/rand"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/Tanibox/tania-server/reservoir"
@@ -10,44 +11,74 @@ import (
 
 // ReservoirRepository is a repository
 type ReservoirRepository interface {
-	FindAll() ([]reservoir.Reservoir, error)
+	FindAll() <-chan RepositoryResult
 	FindByID(uid string) (reservoir.Reservoir, error)
-	Save(val reservoir.Reservoir) (string, error)
+	Save(val *reservoir.Reservoir) <-chan RepositoryResult
+}
+
+// RepositoryResult is a struct to wrap repository result
+// so its easy to use it in channel
+type RepositoryResult struct {
+	Result interface{}
+	Error  error
 }
 
 // ReservoirRepositoryInMemory is in-memory ReservoirRepository db implementation
 type ReservoirRepositoryInMemory struct {
+	mutex        *sync.Mutex
 	ReservoirMap map[string]reservoir.Reservoir
 }
 
 // FindAll is to find all
-func (r ReservoirRepositoryInMemory) FindAll() ([]reservoir.Reservoir, error) {
-	reservoirs := []reservoir.Reservoir{}
+func (r *ReservoirRepositoryInMemory) FindAll() <-chan RepositoryResult {
+	result := make(chan RepositoryResult)
 
-	for _, val := range r.ReservoirMap {
-		reservoirs = append(reservoirs, val)
-	}
+	go func() {
+		r.mutex.Lock()
+		defer r.mutex.Unlock()
 
-	return reservoirs, nil
+		reservoirs := []reservoir.Reservoir{}
+		for _, val := range r.ReservoirMap {
+			reservoirs = append(reservoirs, val)
+		}
+
+		result <- RepositoryResult{Result: reservoirs}
+
+		close(result)
+	}()
+
+	return result
 }
 
 // FindByID is to find by ID
-func (r ReservoirRepositoryInMemory) FindByID(uid string) (reservoir.Reservoir, error) {
+func (r *ReservoirRepositoryInMemory) FindByID(uid string) (reservoir.Reservoir, error) {
+	// Unimplemented
 	return r.ReservoirMap[uid], nil
 }
 
 // Save is to save
-func (r *ReservoirRepositoryInMemory) Save(val reservoir.Reservoir) (UID string, err error) {
-	if r.ReservoirMap == nil {
-		r.ReservoirMap = map[string]reservoir.Reservoir{}
-	}
+func (r *ReservoirRepositoryInMemory) Save(val *reservoir.Reservoir) <-chan RepositoryResult {
+	result := make(chan RepositoryResult)
 
-	uid := getRandomUID()
+	go func() {
+		r.mutex.Lock()
+		defer r.mutex.Unlock()
 
-	val.UID = uid
-	r.ReservoirMap[uid] = val
+		if r.ReservoirMap == nil {
+			r.ReservoirMap = map[string]reservoir.Reservoir{}
+		}
 
-	return uid, nil
+		uid := getRandomUID()
+
+		val.UID = uid
+		r.ReservoirMap[uid] = *val
+
+		result <- RepositoryResult{Result: uid}
+
+		close(result)
+	}()
+
+	return result
 }
 
 func getRandomUID() string {
--- reservoir/repository/reservoir_repository_test.go
+++ reservoir/repository/reservoir_repository_test.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 
 	"github.com/Tanibox/tania-server/reservoir"
@@ -9,7 +11,8 @@ import (
 
 func TestInMemorySave(t *testing.T) {
 	// Given
-	var repo ReservoirRepository = &ReservoirRepositoryInMemory{}
+	var m sync.Mutex
+	var repo ReservoirRepository = &ReservoirRepositoryInMemory{mutex: &m}
 
 	reservoir1, _ := reservoir.CreateReservoir("My Reservoir 1", 8.1, 12.2, 30.0)
 
@@ -17,32 +20,40 @@ func TestInMemorySave(t *testing.T) {
 	bucket2, _ := reservoir.CreateBucket(100, 50)
 	reservoir2.AttachBucket(&bucket2)
 
+	reservoir3, _ := reservoir.CreateReservoir("My Reservoir 3", 8.1, 12.2, 30.0)
+	tap3, _ := reservoir.CreateTap()
+	reservoir3.AttachTap(&tap3)
+
 	// When
-	uid1, _ := repo.Save(reservoir1)
-	uid2, _ := repo.Save(reservoir2)
+	saveResult1 := repo.Save(&reservoir1)
+	saveResult2 := repo.Save(&reservoir2)
+	saveResult3 := repo.Save(&reservoir3)
 
 	// Then
-	assert.NotNil(t, uid1)
-
-	assert.NotNil(t, uid2)
-	assert.NotNil(t, uid2)
+	assert.NotNil(t, saveResult1)
+	assert.NotNil(t, saveResult2)
+	assert.NotNil(t, saveResult3)
 }
 
 func TestInMemoryFindAll(t *testing.T) {
 	// Given
-	var repo ReservoirRepository = &ReservoirRepositoryInMemory{}
+	var m sync.Mutex
+	var repo ReservoirRepository = &ReservoirRepositoryInMemory{mutex: &m}
 
 	reservoir1, _ := reservoir.CreateReservoir("My Reservoir 1", 8.1, 12.2, 30.0)
 	reservoir2, _ := reservoir.CreateReservoir("My Reservoir 2", 8.1, 12.2, 30.0)
 	reservoir3, _ := reservoir.CreateReservoir("My Reservoir 3", 8.1, 12.2, 30.0)
-	repo.Save(reservoir1)
-	repo.Save(reservoir2)
-	repo.Save(reservoir3)
+	repo.Save(&reservoir1)
+	repo.Save(&reservoir2)
+	repo.Save(&reservoir3)
 
 	// When
-	reservoirs, _ := repo.FindAll()
+	results := repo.FindAll()
 
 	// Then
-	assert.Equal(t, len(reservoirs), 3)
-	assert.Equal(t, reservoirs[0].Name, reservoir1.Name)
+	for val := range results {
+		fmt.Println(val.Result)
+	}
+
+	assert.Equal(t, len(results), 3)
 }
