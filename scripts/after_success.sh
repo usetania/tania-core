461a7f735a3401772f98e9149935703088739c7c
Didiet Noor <lynxluna@gmail.com>
2017-12-28 19:44:04 +0700

    Uses 7zip on Windows
    Windows is a little bit different as they don't normally have
    tar tools. So we compress our archive with 7zip.

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -8,7 +8,7 @@ echo -n "Archiving..."
 
 tar cvJf tania-server-${TIMESTAMP}-linux-arm.txz terra.linux.arm public js
 tar cvJf tania-server-${TIMESTAMP}-linux-amd64.txz terra.linux.amd64 public js
-tar cvJf tania-server-${TIMESTAMP}-windows-amd64.txz terra.windows.amd64.exe public js
+7za a -r -t7z tania-server-${TIMESTAMP}-windows-amd64.7z terra.windows.amd64.exe public js
 
 
 if  [ "x$TRAVIS_PULL_REQUEST" == "xfalse" ] 
@@ -18,6 +18,6 @@ then
 
   aws s3 cp tania-server-${TIMESTAMP}-linux-arm.txz s3://tanibox-terra/archives/
   aws s3 cp tania-server-${TIMESTAMP}-linux-amd64.txz s3://tanibox-terra/archives/
-  aws s3 cp tania-server-${TIMESTAMP}-windows-amd64.txz s3://tanibox-terra/archives/
+  aws s3 cp tania-server-${TIMESTAMP}-windows-amd64.7z s3://tanibox-terra/archives/
 
 fi
