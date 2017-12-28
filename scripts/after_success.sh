73c58d4e34ad097856cad9768bcc7224cd679cb3
Didiet Noor <lynxluna@gmail.com>
2017-12-28 19:16:34 +0700

    Uploading to s3

---
+++ scripts/after_success.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+TIMESTAMP="$(date +%Y%m%d-%s)-b${TRAVIS_BUILD_NUMBER}"
+
+echo -n "Archiving..."
+
+tar cvJf tania-server-${TIMESTAMP}-linux-arm.txz terra.linux.arm public js
+tar cvJf tania-server-${TIMESTAMP}-linux-amd64.txz terra.linux.amd64 public js
+tar cvJf tania-server-${TIMESTAMP}-windows-amd64.txz terra.windows.amd64.exe public js
+
+
+if [ "x$TRAVIS_PULL_REQUEST" == "xfalse" ]; then
+
+  echo -n "Uploading to S3..."
+
+  aws s3 cp tania-server-${TIMESTAMP}-linux-arm.txz s3://tanibox-terra/archives/
+  aws s3 cp tania-server-${TIMESTAMP}-linux-amd64.txz s3://tanibox-terra/archives/
+  aws s3 cp tania-server-${TIMESTAMP}-windows-amd64.txz s3://tanibox-terra/archives/
+
+fi
