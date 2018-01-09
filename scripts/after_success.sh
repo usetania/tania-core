2c6ef023062995995b6f6cd1cafb159f696cb5a8
Didiet Noor <lynxluna@gmail.com>
2018-01-10 02:24:55 +0700

    Fixes after success script

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -6,9 +6,9 @@ TIMESTAMP="$(date +%Y%m%d-%s)-b${TRAVIS_BUILD_NUMBER}"
 
 echo -n "Archiving..."
 
-tar cvJf tania-server-${TIMESTAMP}-linux-arm.txz terra.linux.arm public js
-tar cvJf tania-server-${TIMESTAMP}-linux-amd64.txz terra.linux.amd64 public js
-7za a -r -t7z tania-server-${TIMESTAMP}-windows-amd64.7z terra.windows.amd64.exe public js
+tar cvJf tania-server-${TIMESTAMP}-linux-arm.txz terra.linux.arm public
+tar cvJf tania-server-${TIMESTAMP}-linux-amd64.txz terra.linux.amd64 public
+7za a -r -t7z tania-server-${TIMESTAMP}-windows-amd64.7z terra.windows.amd64.exe public
 
 
 if  [ "x$TRAVIS_PULL_REQUEST" == "xfalse" ] 
