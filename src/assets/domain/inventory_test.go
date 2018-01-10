11db69bbeb23454b77970fdf6e1207f48e2e1e02
Adhatama <am.adhatama@gmail.com>
2018-01-10 16:51:05 +0700

    Add simple inventory creation test

--- src/assets/domain/inventory_test.go
+++ src/assets/domain/inventory_test.go
@@ -1,7 +1,19 @@
 package domain
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCreateInventory(t *testing.T) {
+	// When
+	inventoryMaterial1, err1 := CreateInventoryMaterial(Vegetable{}, "Sawi Putih")
+	inventoryMaterial2, err2 := CreateInventoryMaterial(Fruit{}, "Apple")
 
+	// Then
+	assert.Nil(t, err1)
+	assert.NotNil(t, inventoryMaterial1)
+	assert.Nil(t, err2)
+	assert.NotNil(t, inventoryMaterial2)
 }
