719bed6e464a6f0d6354f3c64d6aa39dbe33dd8e
Didiet Noor <lynxluna@gmail.com>
2018-01-24 00:47:14 +0700

    Set configuration and also travis's wget

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -7,6 +7,10 @@ make linux-arm linux-amd64 windows
 
 CURRENT_DIR=$(pwd)
 
+# Setting up configuration
+cp conf.json.example conf.json
+sed -i.bak "s|/Users/user/Code/golang|$TRAVIS_BUILD_DIR/gopath|g" conf.json
+
 # Run golang on linux
 ./terra.linux.amd64 > /dev/null 2>&1 &
 TERRA_PID=$!
@@ -16,9 +20,6 @@ echo "Terra running in the background at pid ${TERRA_PID}"
 # build and run unit test
 yarn && yarn run unit
 
-# Setting up configuration
-cp conf.json.example conf.json
-sed -i.bak "s|/Users/user/Code/golang|$TRAVIS_BUILD_DIR/gopath|g" conf.json
 
 # build and test e2e
 yarn run production && yarn run cypress:run 
