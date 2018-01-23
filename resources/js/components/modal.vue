ef27526cdb0460731fcb20ea0357ba15d70bb388
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2018-01-23 01:00:06 -0500

    using the bootstrap modal sass

--- resources/js/components/modal.vue
+++ resources/js/components/modal.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
-  .modal-mask
-    .modal-wrapper
-      .modal-container
-        slot
-          .modal-header
-            span.h4.font-bold This is the Modal Header
-          .modal-body
-            span.h4.font-bold This is the Modal Body
+  .modal-open
+    .modal.fade.in(style="display: block;")
+      .modal-dialog
+        .modal-content
+          slot
+            .modal-header
+              span.h4.font-bold This is the Modal Header
+            .modal-body
+              span.h4.font-bold This is the Modal Body
+    .modal-backdrop.fade.in
 </template>
 
 <script>
