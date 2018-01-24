171fe8aed547720c7b830d3ef0bb0dca299f4396
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2018-01-23 22:36:00 -0500

    adding escape key to close the modal

--- resources/js/components/modal.vue
+++ resources/js/components/modal.vue
@@ -1,19 +1,28 @@
 <template lang="pug">
-  .modal-open
-    .modal.fade.in(style="display: block;")
-      .modal-dialog
-        .modal-content
-          slot
-            .modal-header
-              span.h4.font-bold This is the Modal Header
-            .modal-body
-              span.h4.font-bold This is the Modal Body
+  .modal-template
+    .modal-open
+      .modal(style="display: block;")
+        .modal-dialog
+          .modal-content
+            slot
+              .modal-header
+                span.h4.font-bold This is the Modal Header
+              .modal-body
+                span.h4.font-bold This is the Modal Body
     .modal-backdrop.fade.in
 </template>
 
 <script>
 export default {
-  name: 'AppModalComponent'
+  name: 'AppModalComponent',
+  created: function () {
+    var vm = this
+    window.addEventListener('keyup', function(e) {
+        if (e.keyCode == 27) {
+          vm.$emit('close')
+        }
+    });
+  }
 }
 </script>
 
