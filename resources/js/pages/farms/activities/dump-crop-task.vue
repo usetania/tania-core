f6314e2c5c9f8c051ae31b9b983794932117b9ec
Kerwin Jorbina <kerwin.jorbina@gmail.com>
2018-01-31 02:06:04 -0500

    dump page

--- resources/js/pages/farms/activities/dump-crop-task.vue
+++ resources/js/pages/farms/activities/dump-crop-task.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   .dump-crop-task(v-if="loading === false")
     .modal-header
-      span.h4.font-bold Move Crops
+      span.h4.font-bold Dump Crops
     .modal-body
       form(@submit.prevent="validateBeforeSubmit")
         .form-group
-          label(for="type") Select area to move this crop
+          label(for="type")
+            | Choose area where you want to dump 
+            span.identifier-sm {{ crop.batch_id }}
           select.form-control#dest_area_id(v-validate="'required'" :class="{'input': true, 'text-danger': errors.has('type') }" v-model="task.dest_area_id" name="dest_area_id")
             option(value="") Please select area
             option(v-for="area in areas" :value="area.uid") {{ area.name }}
@@ -14,7 +16,7 @@
           label(for="quantity")
             | How many of 
             span.identifier-sm {{ crop.batch_id }}
-            |  do you want to move?
+            |  do you want to dump?
           input.form-control#quantity(type="text" v-validate="'required'" :class="{'input': true, 'text-danger': errors.has('quantity') }" v-model="task.quantity" name="quantity")
           span.help-block.text-danger(v-show="errors.has('quantity')") {{ errors.first('quantity') }}
         .form-group
