c19be91d1d19e27a462cafb634b9556cf9529694
Adhatama <am.adhatama@gmail.com>
2018-02-26 21:39:47 +0700

    Add reservoir find all using sqlite

--- src/assets/query/sqlite/reservoir_read_query.go
+++ src/assets/query/sqlite/reservoir_read_query.go
@@ -131,62 +131,100 @@ func (s ReservoirReadQuerySqlite) FindByID(uid uuid.UUID) <-chan query.QueryResu
 func (s ReservoirReadQuerySqlite) FindAllByFarm(farmUID uuid.UUID) <-chan query.QueryResult {
 	result := make(chan query.QueryResult)
 
-	// go func() {
-	// 	farmReads := []storage.FarmRead{}
-	// 	rowsData := reservoirReadResult{}
+	go func() {
+		reservoirReads := []storage.ReservoirRead{}
 
-	// 	rows, err := s.DB.Query("SELECT * FROM FARM_READ ORDER BY CREATED_DATE ASC")
-	// 	if err != nil {
-	// 		result <- query.QueryResult{Error: err}
-	// 		close(result)
-	// 	}
+		rows, err := s.DB.Query("SELECT * FROM RESERVOIR_READ WHERE FARM_UID = ?", farmUID)
+		if err != nil {
+			result <- query.QueryResult{Error: err}
+		}
 
-	// 	for rows.Next() {
-	// 		err = rows.Scan(
-	// 			&rowsData.UID,
-	// 			&rowsData.Name,
-	// 			&rowsData.Latitude,
-	// 			&rowsData.Longitude,
-	// 			&rowsData.Type,
-	// 			&rowsData.CountryCode,
-	// 			&rowsData.CityCode,
-	// 			&rowsData.IsActive,
-	// 			&rowsData.CreatedDate,
-	// 		)
+		for rows.Next() {
+			rowsData := reservoirReadResult{}
+			err = rows.Scan(
+				&rowsData.UID,
+				&rowsData.Name,
+				&rowsData.WaterSourceType,
+				&rowsData.WaterSourceCapacity,
+				&rowsData.FarmUID,
+				&rowsData.FarmName,
+				&rowsData.CreatedDate,
+			)
 
-	// 		if err != nil {
-	// 			result <- query.QueryResult{Error: err}
-	// 			close(result)
-	// 		}
+			if err != nil {
+				result <- query.QueryResult{Error: err}
+			}
 
-	// 		farmUID, err := uuid.FromString(rowsData.UID)
-	// 		if err != nil {
-	// 			result <- query.QueryResult{Error: err}
-	// 			close(result)
-	// 		}
+			reservoirUID, err := uuid.FromString(rowsData.UID)
+			if err != nil {
+				result <- query.QueryResult{Error: err}
+			}
 
-	// 		createdDate, err := time.Parse(time.RFC3339, rowsData.CreatedDate)
-	// 		if err != nil {
-	// 			result <- query.QueryResult{Error: err}
-	// 			close(result)
-	// 		}
+			farmUID, err := uuid.FromString(rowsData.FarmUID)
+			if err != nil {
+				result <- query.QueryResult{Error: err}
+			}
 
-	// 		farmReads = append(farmReads, storage.FarmRead{
-	// 			UID:         farmUID,
-	// 			Name:        rowsData.Name,
-	// 			Latitude:    rowsData.Latitude,
-	// 			Longitude:   rowsData.Longitude,
-	// 			Type:        rowsData.Type,
-	// 			CountryCode: rowsData.CountryCode,
-	// 			CityCode:    rowsData.CityCode,
-	// 			IsActive:    rowsData.IsActive != 0,
-	// 			CreatedDate: createdDate,
-	// 		})
-	// 	}
+			resCreatedDate, err := time.Parse(time.RFC3339, rowsData.CreatedDate)
+			if err != nil {
+				result <- query.QueryResult{Error: err}
+			}
 
-	// 	result <- query.QueryResult{Result: farmReads}
-	// 	close(result)
-	// }()
+			noteRows, err := s.DB.Query("SELECT * FROM RESERVOIR_READ_NOTES WHERE RESERVOIR_UID = ?", reservoirUID)
+			if err != nil {
+				result <- query.QueryResult{Error: err}
+			}
+
+			notes := []storage.ReservoirNote{}
+			for noteRows.Next() {
+				notesRowsData := reservoirNotesReadResult{}
+				err := noteRows.Scan(
+					&notesRowsData.UID,
+					&notesRowsData.ReservoirUID,
+					&notesRowsData.Content,
+					&notesRowsData.CreatedDate,
+				)
+
+				if err != nil {
+					result <- query.QueryResult{Error: err}
+				}
+
+				noteUID, err := uuid.FromString(notesRowsData.UID)
+				if err != nil {
+					result <- query.QueryResult{Error: err}
+				}
+
+				noteCreatedDate, err := time.Parse(time.RFC3339, notesRowsData.CreatedDate)
+				if err != nil {
+					result <- query.QueryResult{Error: err}
+				}
+
+				notes = append(notes, storage.ReservoirNote{
+					UID:         noteUID,
+					Content:     notesRowsData.Content,
+					CreatedDate: noteCreatedDate,
+				})
+			}
+
+			reservoirReads = append(reservoirReads, storage.ReservoirRead{
+				UID:  reservoirUID,
+				Name: rowsData.Name,
+				WaterSource: storage.WaterSource{
+					Type:     rowsData.WaterSourceType,
+					Capacity: rowsData.WaterSourceCapacity,
+				},
+				Farm: storage.ReservoirFarm{
+					UID:  farmUID,
+					Name: rowsData.FarmName,
+				},
+				CreatedDate: resCreatedDate,
+				Notes:       notes,
+			})
+		}
+
+		result <- query.QueryResult{Result: reservoirReads}
+		close(result)
+	}()
 
 	return result
 }
