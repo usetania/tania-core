b502d023364118da204662c4429417b4e1ca79df
Resi Respati <resir014@gmail.com>
2019-01-22 01:13:58 +0700

    attempt to fix webpack build issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,11 @@
 const path = require('path')
-const VueLoaderPlugin = require('vue-loader')
 
 module.exports = {
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'resources/js')
     }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      { 
-        test: /\.hbs$/,
-        use: {
-          loader: 'handlebars-loader'
-        }
-      },
-      {
-        test: /\.pug$/,
-        oneOf: [
-          // this applies to `<template lang="pug">` in Vue components
-          {
-            resourceQuery: /^\?vue/,
-            use: ['pug-plain-loader']
-          },
-          // this applies to pug imports inside JavaScript
-          {
-            use: ['raw-loader', 'pug-plain-loader']
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env']
-          }
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        loader: ['raw-loader', 'sass-loader']
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
+  }
 }
 
 // test specific setups
