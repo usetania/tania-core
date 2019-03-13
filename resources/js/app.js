5d6c8fdfa57e905a9d4ca01f019a6570193f05e4
bepitulaz <bepitulaz@gmail.com>
2019-03-13 17:27:34 +0800

    Setup gettext.
    I install gettext to start working for localisation.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import VueRouter from './router'
 import VuexStore from './stores'
-
 import Component from './component'
 import VeeValidate, { Validator } from 'vee-validate'
-
 import moment from 'moment-timezone'
 import VueMoment from 'vue-moment'
 import vClickOutside from 'v-click-outside'
-
 import Toasted from 'vue-toasted'
-
 import AppComponent from './components/app.vue'
+import GetTextPlugin from 'vue-gettext'
+import translations from '../../languages/translations.json'
+
 import {
   http,
   IsAlphanumSpaceHyphenUnderscore,
@@ -29,10 +28,21 @@ Vue.use(Component)
 Vue.use(VeeValidate)
 Vue.use(VueMoment, { moment })
 Vue.use(vClickOutside)
-Vue.use(Toasted, { 
-   theme: "bubble", 
-   position: "bottom-center", 
+Vue.use(Toasted, {
+   theme: "bubble",
+   position: "bottom-center",
    duration : 1500
+})
+
+Vue.use(GetTextPlugin, {
+  availableLanguages: {
+    en_GB: 'British English',
+    id_ID: 'Bahasa Indonesia',
+    hu_HU: 'Magyar Nyelv'
+  },
+  defaultLanguage: 'en_GB',
+  translations: translations,
+  silent: false
 })
 
 new Vue({
