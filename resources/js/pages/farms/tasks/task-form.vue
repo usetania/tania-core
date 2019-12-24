966c9e62024c994a72a45961d43139fb875179f6
Asep Bagja Priandana <bepitulaz@gmail.com>
2019-12-24 23:48:32 +0800

    Hotfix #98
    This commit is fixing the error notification when creating a task. This is a fix for issue https://github.com/Tanibox/tania-core/issues/98

--- resources/js/pages/farms/tasks/task-form.vue
+++ resources/js/pages/farms/tasks/task-form.vue
@@ -185,7 +185,7 @@ export default {
       }
       this.submitTask(this.task)
         .then(() => this.$parent.$emit('close'))
-        .catch(() => this.$toasted.error('Error in task submission'));
+        .catch(() => this.$toasted.error('Task due date cannot be earlier than the current date.'));
     },
   },
 };
