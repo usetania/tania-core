c7fcfade14668fe2114fcb9ac659df4a279a5853
Yasin Junet <yasintze@gmail.com>
2021-09-05 19:48:59 +0700

    styling login page

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Dashboard: NextPage = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,85 @@
+import { useState } from "react";
 import type { NextPage } from "next";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { FaUnlock } from "react-icons/fa";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameIsEmpty, setUsernameIsEmpty] = useState(false);
+  const [passwordIsEmpty, setPasswordIsEmpty] = useState(false);
+
+  const handleSubmit = () => {
+    if (!username || !password) {
+      setUsernameIsEmpty(!username);
+      setPasswordIsEmpty(!password);
+    } else {
+      router.push("dashboard");
+    }
+  };
+
   return (
-    <>
-      <main>
-        <Container>
-          <Row>
-            <Col>
-              <Button variant="primary">Primary</Button>
-            </Col>
-          </Row>
-        </Container>
-      </main>
-    </>
+    <div className="bg-gray d-flex align-items-center vh-100">
+      <Container fluid>
+        <Row>
+          <Col sm={12} md={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 4 }}>
+            <Card>
+              <Card.Body>
+                <div className="text-center">
+                  <Image
+                    src={"/images/logobig.png"}
+                    layout="fixed"
+                    width={200}
+                    height={65}
+                  />
+                </div>
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Input your username here"
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                    {usernameIsEmpty && (
+                      <Form.Text className="text-danger">
+                        The username field is required
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Your password here"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {passwordIsEmpty && (
+                      <Form.Text className="text-danger">
+                        The password field is required
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                </Form>
+                <div className="text-center">
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    onClick={handleSubmit}
+                  >
+                    <FaUnlock className="me-1" />
+                    Login
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
