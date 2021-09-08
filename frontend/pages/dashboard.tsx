62f7d5776288ce580c067cbfa4658c8ee66a89f7
Yasin Junet <yasintze@gmail.com>
2021-09-08 20:25:59 +0700

    refactor dashboard panel

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Card, Col, Form, Nav, Row, Table } from "react-bootstrap";
 
 import Layout from "../components/Layout";
+import Panel from "../components/Panel";
 import TableTaskItem from "../components/TableTaskItem";
 import { dashboardData, cropsData, tasksData } from "../data";
 
@@ -15,106 +16,92 @@ const Dashboard: NextPage = () => {
         </Col>
       </Row>
       <Row>
-        <Col xs={12} sm={12} md={6}>
-          <Card className="mb-3">
-            <Card.Body>
-              <Card.Title>
-                <h5>At A Glance</h5>
-              </Card.Title>
-              <Nav className="flex-column">
-                {dashboardData.map(({ name, route, icon }) => (
-                  <Nav.Item key={name} className="mb-1">
-                    <div className="d-flex align-items-center">
-                      {icon}
-                      <Link href={route}>{name}</Link>
-                    </div>
-                  </Nav.Item>
-                ))}
-              </Nav>
-            </Card.Body>
-            <Card.Footer>You are using Tania 1.7 right now</Card.Footer>
-          </Card>
-        </Col>
-        <Col xs={12} sm={12} md={6}>
-          <Card className="mb-3">
-            <Card.Body>
-              <Card.Title>
-                <h5>What is on Production</h5>
-              </Card.Title>
-              <div className="mb-3">
-                <Link href="/crops">See all Crops</Link>
-              </div>
-              <Table responsive>
-                <thead>
-                  <tr>
-                    <th>Varieties</th>
-                    <th>Qty</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cropsData &&
-                    cropsData.map(({ id, varieties, qty }) => (
-                      <tr key={id}>
-                        <td>
-                          <Link href={`/crops/${id}`}>{varieties}</Link>
-                        </td>
-                        <td>{qty}</td>
-                      </tr>
-                    ))}
-                </tbody>
-              </Table>
-            </Card.Body>
-          </Card>
-        </Col>
+        <Panel
+          title="At A Glance"
+          md={6}
+          footer="You are using Tania 1.7 right now"
+        >
+          <Nav className="flex-column">
+            {dashboardData.map(({ name, route, icon }) => (
+              <Nav.Item key={name} className="mb-1">
+                <div className="d-flex align-items-center">
+                  {icon}
+                  <Link href={route}>{name}</Link>
+                </div>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Panel>
+        <Panel title="What is on Production" md={6}>
+          <>
+            <div className="mb-3">
+              <Link href="/crops">See all Crops</Link>
+            </div>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Varieties</th>
+                  <th>Qty</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cropsData &&
+                  cropsData.map(({ id, varieties, qty }) => (
+                    <tr key={id}>
+                      <td>
+                        <Link href={`/crops/${id}`}>{varieties}</Link>
+                      </td>
+                      <td>{qty}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          </>
+        </Panel>
       </Row>
       <Row>
-        <Col xs={12} sm={12} md={12}>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <h5>Tasks</h5>
-              </Card.Title>
-              <div className="mb-3">
-                <Link href="/tasks">See all Tasks</Link>
-              </div>
-              <Table responsive>
-                <thead>
-                  <tr>
-                    <th></th>
-                    <th className="w-75">Items</th>
-                    <th>Category</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tasksData &&
-                    tasksData.map(
-                      ({ id, item, details, dueDate, priority, category }) => (
-                        <tr key={id}>
-                          <td>
-                            <Form>
-                              <Form.Check type="checkbox" />
-                            </Form>
-                          </td>
-                          <td>
-                            <TableTaskItem
-                              id={id}
-                              item={item}
-                              details={details}
-                              dueDate={dueDate}
-                              priority={priority}
-                            />
-                          </td>
-                          <td>
-                            <span className="text-uppercase">{category}</span>
-                          </td>
-                        </tr>
-                      )
-                    )}
-                </tbody>
-              </Table>
-            </Card.Body>
-          </Card>
-        </Col>
+        <Panel title="Tasks">
+          <>
+            <div className="mb-3">
+              <Link href="/tasks">See all Tasks</Link>
+            </div>
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th className="w-75">Items</th>
+                  <th>Category</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasksData &&
+                  tasksData.map(
+                    ({ id, item, details, dueDate, priority, category }) => (
+                      <tr key={id}>
+                        <td>
+                          <Form>
+                            <Form.Check type="checkbox" />
+                          </Form>
+                        </td>
+                        <td>
+                          <TableTaskItem
+                            id={id}
+                            item={item}
+                            details={details}
+                            dueDate={dueDate}
+                            priority={priority}
+                          />
+                        </td>
+                        <td>
+                          <span className="text-uppercase">{category}</span>
+                        </td>
+                      </tr>
+                    )
+                  )}
+              </tbody>
+            </Table>
+          </>
+        </Panel>
       </Row>
     </Layout>
   );
